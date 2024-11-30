a8d2b7269c8f0eeaf4d336f6f5a0325a013d7f0d
tracer-nju <tracer@nju.edu.cn>
2024-11-30 23:29:52 +0800

    Linux LAPTOP-8TQI1NBK 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:29:52 up  1:15,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -17,6 +17,19 @@ void show_usage() {
          "-V, --version        dump version info");
 }
 
+
+void print_version() {
+  printf("pstree 0.0.1\n");
+}
+
+void show_pids() {
+
+}
+
+void sort_pids() {
+  
+}
+
 int main(int argc, char *argv[]) {
 
   int ch;
@@ -24,9 +37,12 @@ int main(int argc, char *argv[]) {
   while ((ch = getopt_long(argc, argv, short_opts, long_opts, NULL)) != -1) {
     switch (ch) {
     case 'p':
+      show_pids();
+      break;
     case 'n':
+      sort_pids();
     case 'V':
-      opt=optarg;
+      print_version();
       break;
     default:
       printf("parse args failed, input p / n / V\n");

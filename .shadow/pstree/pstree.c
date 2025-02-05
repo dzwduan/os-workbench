0ff4f573baab1f186e4191a3fda0b27eb420e28e
tracer-nju <tracer@nju.edu.cn>
2025-02-06 00:27:55 +0800

    Linux LAPTOP-8TQI1NBK 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:27:55 up  1:26,  1 user,  load average: 0.11, 0.09, 0.02

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,26 +1,27 @@
 #include <assert.h>
 #include <bits/getopt_core.h>
-#include <string.h>
+#include <ctype.h>
+#include <dirent.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <dirent.h>
-#include <ctype.h>
 
 #define FILE_TYPE 8
-#define DIR_TYPE  4
+#define DIR_TYPE 4
 
 #define MAX_SIZE 1024
 
-int pid_list[MAX_SIZE] = {}; 
-char * pid_names[MAX_SIZE] = {};
-int ppid_list[MAX_SIZE] = {};
+// int pid_list[MAX_SIZE] = {};
+// char *pid_names[MAX_SIZE] = {};
+// int ppid_list[MAX_SIZE] = {};
 static int pid_idx = 0;
+int tree_index_table[MAX_SIZE] = {};
 
-const char *short_opts = "pnV";
+const char *short_opts = "pn:V";
 struct option long_opts[] = {{"show-pids", no_argument, NULL, 'p'},
-                             {"numeric-sort", no_argument, NULL, 'n'},
+                             {"numeric-sort", required_argument, NULL, 'n'},
                              {"version", no_argument, NULL, 'V'}};
 
 void show_usage() {
@@ -31,33 +32,34 @@ void show_usage() {
          "-V, --version        dump version info");
 }
 
+void print_version() { fprintf(stderr, "pstree 0.0.1\n"); }
 
-void print_version() {
-  fprintf(stderr, "pstree 0.0.1\n");
-}
-
-int is_digit(char * s) {
-  if (s==NULL) return 0;
-  for(int i = 0; s[i]; i++) {
-    if (!isdigit(s[i])) return 0;
+int is_digit(char *s) {
+  if (s == NULL)
+    return 0;
+  for (int i = 0; s[i]; i++) {
+    if (!isdigit(s[i]))
+      return 0;
   }
   return 1;
 }
 
-char * get_name_by_pid(pid_t pid) {
+char *get_name_by_pid(pid_t pid) {
   char pid_path[32];
   char buf[128];
   char *name = malloc(128);
-  if (name == NULL) exit(1);
+  if (name == NULL)
+    exit(1);
   sprintf(pid_path, "/proc/%d/status", pid);
-  FILE* fp = fopen(pid_path, "r");
-  if (fp==NULL) exit(1);
-  if(fgets(buf, sizeof(buf)-1, fp) != NULL) {
+  FILE *fp = fopen(pid_path, "r");
+  if (fp == NULL)
+    exit(1);
+  if (fgets(buf, sizeof(buf) - 1, fp) != NULL) {
     sscanf(buf, "Name:   %s", name);
   }
 
   fclose(fp);
-  //printf("pid = %d, name = %s\n", pid, name);
+  // printf("pid = %d, name = %s\n", pid, name);
   return name;
 }
 
@@ -66,8 +68,9 @@ pid_t get_ppid_by_pid(pid_t pid) {
   char buf[128];
   pid_t ppid = -1;
   sprintf(pid_path, "/proc/%d/status", pid);
-  FILE* fp = fopen(pid_path, "r");
-  if (fp == NULL) exit(1);
+  FILE *fp = fopen(pid_path, "r");
+  if (fp == NULL)
+    exit(1);
   while (fgets(buf, sizeof(buf) - 1, fp) != NULL) {
     if (sscanf(buf, "PPid: %d", &ppid) == 1) {
       break;
@@ -77,41 +80,103 @@ pid_t get_ppid_by_pid(pid_t pid) {
   return ppid;
 }
 
-void show_pids() {
- struct dirent *f;
-  DIR * dirp = opendir("/proc");
-  if (dirp==NULL) {
+int *get_ppid_list(int ppid_len) {
+  int *ppid_list = malloc(ppid_len * sizeof(int));
+  if (ppid_list == NULL) {
+    perror("malloc");
+    exit(1);
+  }
+  for (int i = 0; i < ppid_len; i++) {
+    ppid_list[i] = get_ppid_by_pid(i);
+  }
+
+  return ppid_list;
+}
+
+int *get_pid_list(int pid_len) {
+  int *pid_list = malloc(pid_len * sizeof(int));
+  if (pid_list == NULL) {
+    perror("malloc");
+    exit(1);
+  }
+  for (int i = 0; i < pid_len; i++) {
+    pid_list[i] = i;
+  }
+
+  return pid_list;
+}
+
+char **get_pid_name_list(int pid_len) {
+  // 默认name最大长度128
+  char **pid_name_list = malloc(pid_len * sizeof(char *) * 128);
+  if (pid_name_list == NULL) {
+    perror("malloc");
+    exit(1);
+  }
+  for (int i = 0; i < pid_len; i++) {
+    pid_name_list[i] = get_name_by_pid(i);
+  }
+
+  return pid_name_list;
+}
+
+int get_pid_len() {
+  struct dirent *f;
+  DIR *dirp = opendir("/proc");
+  if (dirp == NULL) {
     printf("failed open /proc\n");
     exit(1);
   }
 
-  while((f = readdir(dirp))!=NULL) {
-    if (f->d_type==DIR_TYPE) {
+  int pid_len = 0;
+  while ((f = readdir(dirp)) != NULL) {
+    if (f->d_type == DIR_TYPE) {
       if (is_digit(f->d_name)) {
-        int pid = atoi(f->d_name);
-        char name[32];
-        pid_list[pid_idx] = pid;
-        ppid_list[pid_idx] = get_ppid_by_pid(pid);
-        pid_names[pid_idx] = get_name_by_pid(pid);
-        pid_idx++;
+        pid_len++;
       }
     }
   }
 
-  //TODO: dump tree by pid parent and children
-
   closedir(dirp);
-  for (int i = 0; i<pid_idx; i++) {
+  return pid_len;
+}
+
+int get_root(int *pid_list, int *ppid_list) {
+  int root_pid = -1;
+  for (int i = 0; i < pid_idx; i++) {
+    for (int j = 0; j < pid_idx; j++) {
+      if (ppid_list[i] == pid_list[j]) {
+        break;
+      }
+      if (j == pid_idx - 1) {
+        root_pid = pid_list[i];
+      }
+    }
+  }
+  return root_pid;
+}
+
+void show_pids() {
+  int pid_len = get_pid_len();
+  int *pid_list = get_pid_list(pid_len);
+  int *ppid_list = get_ppid_list(pid_len);
+  char **pid_names = get_pid_name_list(pid_len);
+
+
+  for (int i = 0; i < pid_idx; i++) {
     printf("%s - %d \n", pid_names[i], pid_list[i]);
   }
 }
 
-void sort_pids() {
-
-}
+// dump tree by num pid as root
+void sort_pids(int num) {}
 
 int main(int argc, char *argv[]) {
-
+  assert(argc >= 1);
+  if (argc == 1) {
+    show_usage();
+    return 0;
+  }
   int ch;
   char *opt;
   while ((ch = getopt_long(argc, argv, short_opts, long_opts, NULL)) != -1) {
@@ -120,7 +185,8 @@ int main(int argc, char *argv[]) {
       show_pids();
       break;
     case 'n':
-      sort_pids();
+      assert(is_digit(optarg));
+      sort_pids(atoi(optarg));
     case 'V':
       print_version();
       break;
